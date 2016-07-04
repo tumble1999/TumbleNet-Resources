faaf3a91dba393c4da0c9a4222c9d828138b4568
Tumble <tumblegamer@tumblenet.tk>
2016-07-04 13:04:38 +0100

    Update github.js

--- javascript/jquery/json/github.js
+++ javascript/jquery/json/github.js
@@ -1,4 +1,8 @@
 function githubAPI(location, callback) {
-  var url = "https://api.github.com/" + location + "?callback=?";
-  jQuery.getJSON(url, callback);
+  var url = "https://api.github.com/" + location;
+  // Create a new request object
+var request = new XMLHttpRequest();
+request.onload = callback(request.response);
+request.open('get', url)
+request.send()
 }
