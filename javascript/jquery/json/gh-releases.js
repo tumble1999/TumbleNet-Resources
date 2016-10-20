c96d85ba18eaf34f01c3953140fef426ef5f69c5
Tumble <tumblegamer@tumblenet.tk>
2016-10-20 10:27:44 +0100

    Update gh-releases.js

--- javascript/jquery/json/gh-releases.js
+++ javascript/jquery/json/gh-releases.js
@@ -11,9 +11,10 @@ jQuery.fn.loadRepositories = function(username, repoName) {
         text = text+"<a href='https://github.com/" + username + "/" + repoName + "/repositories/new'><button class='mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent' id='add'><i class='material-icons' role='presentation'>add</i><span class='visuallyhidden'>Add</span></button></a>";
         $(repos).each(function() {
             text = text + "<li>",
-              this.name,
-              this.tag_name
-          "<a href='" +  + "'>More",
+             "<a href='" + this.html_url + "'>"this.name"</a>",
+              this.tag_name,
+              " -  DOWNLOAD: <a href='" + this.zipball_url + "'>Zip</a> ",
+                 "<a href='" + this.tarball_url + "'>Tar</a>"
               
               "</li>";
         });
