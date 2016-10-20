433a87990efe694a19bce51ab0ea1d75acfad523
Tumble <tumblegamer@tumblenet.tk>
2016-10-20 13:15:54 +0100

    Update gh-releases.js

--- javascript/jquery/json/gh-releases.js
+++ javascript/jquery/json/gh-releases.js
@@ -8,7 +8,7 @@ function loadReleases(username, repoName) {
         var releases = data.data; // JSON Parsing
       //https://github.com/tumblegamer/TumbleCraft/releases/new
         text = text+"<a href='https://github.com/" + username + "/" + repoName + "/repositories/new'><button class='mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored mdl-shadow--4dp mdl-color--accent' id='add'><i class='material-icons' role='presentation'>add</i><span class='visuallyhidden'>Add</span></button></a>";
-        $(repos).each(function() {
+        $(releases).each(function() {
             text = text + "<li>",
              "<a href='" + this.html_url + "'>"this.name"</a>",
               this.tag_name,
