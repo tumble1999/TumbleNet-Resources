4445c5be0a46c3fa236eb447e1bc3062e9a3db25
Tumble <tumblegamer@tumblenet.tk>
2016-10-20 13:46:55 +0100

    Update gh-releases.js

--- javascript/jquery/json/gh-releases.js
+++ javascript/jquery/json/gh-releases.js
@@ -7,10 +7,11 @@ jQuery.fn.loadReleases = function(username, repoName) {
     githubAPI("repos/" + username + "/" + repoName + "/releases", function(data) {
         var releases = data.data; // JSON Parsing
       //https://github.com/tumblegamer/TumbleCraft/releases/new
-        text = text+"<a href='https://github.com/" + username + "/" + repoName + "/releases/new'>Add</button></a>";
+        
         $(releases).each(function() {
-            text = text + "<li><a href='" + this.html_url + "'>" + this.name + "</a>" + this.tag_name + " -  DOWNLOAD: <a href='" + this.zipball_url + "'>Zip</a></li>";
+            text = text + "<tr><th><a href='" + this.html_url + "'>" + this.name + "</a></th><th>" + this.tag_name + "</th><th><a href='" + this.zipball_url + "'>Zip</a> - <a href='" + this.tarball_url + "'>Tar</a></th>";
         });
+        text = "<a href='https://github.com/" + username + "/" + repoName + "/releases/new'>Add</button></a>" + "<table style="width:100%"><tr><th>Version Name</th><th>Version Number</th> <th>Download</th></tr>" + text + "</table>";
         target.html(text);
     });
 };
